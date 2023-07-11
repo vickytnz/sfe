2a45367556837db873b7c9b72403738a37cc8009
vickytnz <vicky.teinaki@gmail.com>
2023-07-11 20:28:47 +0100

    tidied up the formatting

--- app/filters.js
+++ app/filters.js
@@ -1,2 +1,12 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
+
+
+addFilter('thousands', function (content) {
+      var thousands = content.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      return thousands;
+  })
+
+addFilter('pound', function (content) {
+  return "£" +  content
+})
